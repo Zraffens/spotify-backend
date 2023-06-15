20ec518bb2508c670a970dcf38a0052898ede308
zraffens <abhinavacharya486@gmail.com>
2023-06-16 00:02:31 +0545

    added vercel.json and other changes

--- spotify/settings.py
+++ spotify/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'desktop-4s2i8gk']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'desktop-4s2i8gk', '.vercel.app']
 
 
 # Application definition
