35572e18450cab555c0cd291c8cb274a7f8a4700
zraffens <abhinavacharya486@gmail.com>
2023-06-15 17:04:31 +0545

    likedDetails field added in serializer

--- myuser/serializers.py
+++ myuser/serializers.py
@@ -3,21 +3,27 @@ from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from .models import MyUser
 from album.serializers import PlaylistSerializer
+from song.serializers import SongSerializer
 from album.models2 import Playlist
 
 
 class UserSerializer(serializers.ModelSerializer):
     playlists = serializers.SerializerMethodField()
+    likedDetails = serializers.SerializerMethodField()
 
     class Meta:
         model = MyUser
-        fields = ('id', 'username', 'email', 'created', 'liked', 'playlists')
+        fields = ('id', 'username', 'email', 'created', 'liked', 'playlists', 'likedDetails')
 
     def get_playlists(self, obj):
         playlists = Playlist.objects.filter(created_by=obj)
         serializer = PlaylistSerializer(playlists, many=True)
         return serializer.data
 
+    def get_likedDetails(self, obj):
+        liked = obj.liked.all()
+        song_serializer = SongSerializer(liked, many=True)
+        return song_serializer.data
 
 class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
